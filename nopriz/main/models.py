81dcc6817b8294bd212db63391a9e6dad1775ebc
pahmaton <truhlyaev24@gmail.com>
2023-11-15 12:36:24 +0300

    поменял birthdate на charfield

--- nopriz/main/models.py
+++ nopriz/main/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class User(models.Model):
     code = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    birthdate = models.CharField(max_length=200)
