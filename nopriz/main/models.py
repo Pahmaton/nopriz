ca88ee0de82907514e64756e3fc1e3f37df062b1
pahmaton <truhlyaev24@gmail.com>
2023-11-16 01:35:25 +0300

    навел красоты в админке

--- nopriz/main/models.py
+++ nopriz/main/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    code = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    birthdate = models.CharField(max_length=200)
+    code = models.CharField(max_length=200, verbose_name='Идентификационный номер')
+    name = models.CharField(max_length=200, verbose_name='ФИО')
+    birthdate = models.CharField(max_length=200, verbose_name='Дата')
+
+    def __str__(self):
+        return f"{self.name} ({self.birthdate}): {self.code} "
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
